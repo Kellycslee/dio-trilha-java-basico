09ac61838c9dcb182f5e921e6e7c93164c1aa44c
Kelly Souza Lee <souza.kelly@hotmail.com>
2023-03-13 11:35:30 -0300

    Projetos e exercicios Java

---
+++ tipos-variaveis/src/CalculadoraSoma.java
@@ -0,0 +1,21 @@
+
+public class CalculadoraSoma {
+    /**
+ * <h1>CalculadoraJ<h1>
+ * A Calculaora realiza operações matemáticas entre númerios inteiros
+ * <p>
+ * <b>Note:<b> Leia atentamente a documetação desta classe para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Gleyson Sampaio
+ * @version 1.0
+ * @since 01/01/2022
+ * Este método é utilizado para somar dois números inteiros
+ * @param numeroUm este é o primeiroparâmetro do método
+ * @param numeroDois este é o segundo parâmetro do método
+ * @return in o resultado deste método é a soma dos dois números
+ */
+ public int somar(int numeroUm, int numeroDois) {
+    return numeroUm + numeroDois;
+ }
+
+}
---
+++ tipos-variaveis/src/Cometarios.java
@@ -0,0 +1,35 @@
+public class Cometarios {
+    
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+
+         */
+    }
+
+    /**
+     * estas duas estrelinhas e/ou asteriscos acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação.
+     * Que incrível !!!
+     */
+
+     public void metodo() {
+
+        /*
+         * Este método foi elaborarado as pressas
+         * por isso eu abreviei o nome das variaveis
+         * mas olha, ele tem a finalidade somar ou multiplicar
+         * dois numeros
+         */
+
+         /*
+          * Esse tipo de exemplo de cometário acima náo deve ser utilizado para detalhar qualquer método mal elaborado
+          */
+     }
+}
